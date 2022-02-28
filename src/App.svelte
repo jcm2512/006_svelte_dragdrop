edf6b4891c3dd78f540069684ef841fd09b3b0d7
jcm2512 <joe.magalotti@gmail.com>
2022-03-01 01:24:46 +0900

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -14,17 +14,16 @@
   );
   function dragMoveListener(event) {
     var target = event.target,
-      // keep the dragged position in the data-x/data-y attributes
-      x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
-      y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+      // keep the dragged position in the x y attributes
+      x = (parseFloat(target.getAttribute("x")) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute("y")) || 0) + event.dy;
 
     // translate the element
-    target.style.webkitTransform = target.style.transform =
-      "translate(" + x + "px, " + y + "px)";
+    target.style.transform = `translate(${x}px, ${y}px)`;
 
     // update the posiion attributes
-    target.setAttribute("data-x", x);
-    target.setAttribute("data-y", y);
+    target.setAttribute("x", x);
+    target.setAttribute("y", y);
 
     // update the z-index
   }
