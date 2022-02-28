4d971671a1577210b57f424450971aa07202e726
jcm2512 <joe.magalotti@gmail.com>
2022-03-01 02:36:17 +0900

    revert position

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,66 @@
 <script>
   import interact from "interactjs";
   export let title;
+  export let word;
 
   const position = { x: 0, y: 0 };
 
   // target elements with the "draggable" class
-  interact(".draggable").draggable(
-    // options
-    {
-      inertia: false,
-      onmove: dragMoveListener,
+  interact(".draggable").draggable({
+    inertia: false,
+    onmove: dragMoveListener,
+    onstart: dragStartListener,
+    onend: dragEndListener,
+  });
+
+  function dragEndListener(event) {
+    const object = event.target;
+
+    // return object to previous location
+    if (object.getAttribute("count") > 0) {
+      // get start position
+      let sx = object.getAttribute("start-x");
+      let sy = object.getAttribute("start-x");
+
+      // translate object and update position attributes
+      object.style.transform = `translate(${sx}px, ${sy}px)`;
+      object.setAttribute("x", sx);
+      object.setAttribute("y", sy);
+
+      // add transition class
+      object.classList.add("transition");
+
+      // remove transition when complete
+      object.addEventListener("transitionend", () => {
+        object.classList.remove("transition");
+      });
     }
-  );
+  }
+
+  function dragStartListener(event) {
+    const object = event.target;
+
+    // store the initial position attributes
+    object.setAttribute("start-x", object.getAttribute("x") || 0);
+    object.setAttribute("start-y", object.getAttribute("y") || 0);
+
+    // count the times dragged
+    object.setAttribute(
+      "count",
+      parseFloat(object.getAttribute("count")) + 1 || 1
+    );
+  }
+
   function dragMoveListener(event) {
-    var target = event.target,
-      // keep the dragged position in the x y attributes
+    const target = event.target,
+      // get the updated dragged position
       x = (parseFloat(target.getAttribute("x")) || 0) + event.dx,
       y = (parseFloat(target.getAttribute("y")) || 0) + event.dy;
 
     // translate the element
     target.style.transform = `translate(${x}px, ${y}px)`;
 
-    // update the posiion attributes
+    // update the position attributes
     target.setAttribute("x", x);
     target.setAttribute("y", y);
 
@@ -30,6 +69,8 @@
 </script>
 
 <main>
+  <div class="transition" style="display: none">d</div>
+
   <h1>{title}!</h1>
   <section class="drag-container">
     <div class="draggable">a</div>
@@ -79,5 +120,10 @@
     padding: 4%;
     touch-action: none;
     user-select: none;
+  }
+
+  .transition {
+    opacity: 0.5;
+    transition: transform 0.3s ease-out;
   }
 </style>
