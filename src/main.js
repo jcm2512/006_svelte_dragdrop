8e35b8eca76271623890542064f4895d145b03e1
jcm2512 <joe.magalotti@gmail.com>
2022-03-06 22:41:24 +0900

    removed highlighting on match

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.svelte";
 const app = new App({
   target: document.body,
   props: {
-    word: "magalotti",
+    words: ["magalotti", "haruka", "tokky", "manju", "nuts", "sherlock"],
   },
 });
 
