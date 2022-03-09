7ff865ed2d35d62ba73b833003871279e6e48c7f
jcm2512 <joe.magalotti@gmail.com>
2022-03-09 11:47:14 +0900

    reduced wordset for testing purposes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from "./App.svelte";
 const app = new App({
   target: document.body,
   props: {
-    words: ["magalotti", "haruka", "tokky", "manju", "nuts", "sherlock"],
+    words: ["tokky", "manju", "nuts"],
   },
 });
 
