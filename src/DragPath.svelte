ec7c83acbccb7789c113e33b04fe0b7d3fa31fc5
jcm2512 <joe.magalotti@gmail.com>
2022-03-15 22:20:15 +0900

    update performance issues

--- src/DragPath.svelte
+++ src/DragPath.svelte
@@ -28,23 +28,27 @@
     // store the initial position attributes
     object.setAttribute("start-x", object.getAttribute("x") || 0);
     object.setAttribute("start-y", object.getAttribute("y") || 0);
+
+    object.setAttribute("offsetWidth", object.offsetWidth / 2);
+    object.setAttribute("offsetHeight", object.offsetHeight / 2);
   }
 
   function dragMoveListener(event) {
     const target = event.target,
-      // get the updated dragged position
-      x = (parseFloat(target.getAttribute("x")) || 0) + event.dx,
-      y = (parseFloat(target.getAttribute("y")) || 0) + event.dy;
+      pathTarget = closestPoint(path, [event.clientX, event.clientY]),
+      x = pathTarget[0] - target.getAttribute("offsetWidth"),
+      y = pathTarget[1] - target.getAttribute("offsetHeight");
+    // get the updated dragged position
+    // x = (parseFloat(target.getAttribute("x")) || 0) + event.dx,
+    // y = (parseFloat(target.getAttribute("y")) || 0) + event.dy;
 
     // translate the element
     // target.style.transform = `translate(${x}px, ${y}px)`;
-    target.style.transform = `translate(${
-      closestPoint(path, [event.clientX, event.clientY])[0]
-    }px, ${closestPoint(path, [event.clientX, event.clientY])[1]}px`;
+    target.style.transform = `translate(${x}px, ${y}px`;
 
     // update the position attributes
-    target.setAttribute("x", x);
-    target.setAttribute("y", y);
+    // target.setAttribute("x", x);
+    // target.setAttribute("y", y);
   }
 </script>
 
@@ -62,18 +66,25 @@
         class="path"
         d="M159 70c-50.386 35.428-74.284 72.547-71.691 111.355 2.592 38.81 31.514 76.92 86.765 114.333L447.7 84.137l-9.812 263.996"
       />
-      <g id="drag">
-        <circle class="oval" cx="15" cy="15" r="15" />
-        <polygon class="arrow" points="10,10 25,15 10,20" />
-      </g>
     </svg>
   </div>
 
   {#each array as item, index}
-    <div bind:this={divs[index]}>
+    <!-- <div bind:this={divs[index]}>
       {item}
+    </div> -->
+    <div class="draggable">
+      <svg height="100" width="100">
+        <circle
+          cx="50"
+          cy="50"
+          r="40"
+          stroke="black"
+          stroke-width="3"
+          fill="red"
+        />
+      </svg>
     </div>
-    <div class="draggable">O</div>
   {/each}
 </main>
 
@@ -92,14 +103,18 @@
   }
 
   .draggable {
-    will-change: transform;
+    /* will-change: transform; */
     position: absolute;
     top: 0;
     left: 0;
-    padding: 2em;
+    /* padding: 2em; */
     z-index: 9;
-    background-color: #f1f1f1;
-    border: 1px solid #d3d3d3;
+    /* background-color: #f1f1f1; */
+    /* border: 1px solid #d3d3d3; */
     text-align: center;
+  }
+
+  :global(body) {
+    padding: 0;
   }
 </style>
