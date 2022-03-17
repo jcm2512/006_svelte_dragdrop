5252576c36139213fc01529d739b3fa35ef40dff
jcm2512 <joe.magalotti@gmail.com>
2022-03-18 02:26:40 +0900

    multiple paths and goals

--- src/gsap.svelte
+++ src/gsap.svelte
@@ -6,7 +6,11 @@
 
   export let paths;
 
-  console.log(paths);
+  // store references to DOM elements
+  let pathElements = [];
+  let dragElements = [];
+  let goalElements = [];
+  let lineElements = [];
 
   onMount(() => {
     // Closest Point on Path
@@ -16,63 +20,101 @@
 
     var DEG = 180 / Math.PI;
 
-    var drag = document.querySelector("#drag");
-    var path = document.querySelector("#path");
+    // LOOP START
+    let drawTween = [];
+    let draggable = [];
 
-    var pathLength = path.getTotalLength() || 0;
-    var startPoint = path.getPointAtLength(0);
-    var startAngle = getRotation(startPoint, path.getPointAtLength(0.1));
-    var lastPoint = path.getPointAtLength(0);
-    var lastLength = 0;
+    for (const [index, value] of paths.entries()) {
+      let drag = dragElements[index],
+        goal = goalElements[index],
+        path = pathElements[index],
+        line = lineElements[index];
 
-    var drawTween = gsap.from("#drawMe", {
-      duration: 1, // migrated to GSAP 3
-      drawSVG: "0%",
-      paused: true,
-      ease: "none", // migrated to GSAP 3
-    });
+      let pathLength = path.getTotalLength() || 0,
+        startPoint = path.getPointAtLength(0),
+        startAngle = getRotation(startPoint, path.getPointAtLength(0.1)),
+        lastPoint = path.getPointAtLength(0),
+        endPoint = path.getPointAtLength(path.getTotalLength()),
+        lastLength = 0;
 
-    gsap.set(drag, {
-      transformOrigin: "center",
-      rotation: startAngle + "_rad",
-      xPercent: -50,
-      yPercent: -50,
-      x: startPoint.x,
-      y: startPoint.y,
-    });
+      drawTween[index] = gsap.from(line, {
+        duration: 1, // migrated to GSAP 3
+        drawSVG: "0%",
+        paused: true,
+        ease: "none", // migrated to GSAP 3
+      });
 
-    var draggable = new Draggable(drag, {
-      liveSnap: {
-        points: pointModifier,
-      },
-    });
+      gsap.set(drag, {
+        transformOrigin: "center",
+        rotation: startAngle + "_rad",
+        xPercent: -50,
+        yPercent: -50,
+        x: startPoint.x,
+        y: startPoint.y,
+      });
+
+      gsap.set(goal, {
+        transformOrigin: "center",
+        xPercent: -50,
+        yPercent: -50,
+        x: endPoint.x,
+        y: endPoint.y,
+      });
+
+      draggable[index] = new Draggable(drag, {
+        liveSnap: {
+          points: function (point) {
+            var p = closestPoint(
+              pathElements[index],
+              pathLength,
+              point,
+              lastPoint,
+              lastLength
+            );
+            gsap.set(drag, {
+              rotation: p.rotation,
+            });
+
+            lastPoint.x = point.x;
+            lastPoint.y = point.y;
+            lastLength = p.length;
+
+            drawTween[index].progress(lastLength / pathLength);
+
+            return p.point;
+          },
+        },
+        onDragEnd: function (e) {
+          // hide draggable when goal is reached
+          if (this.hitTest(goalElements[index])) {
+            gsap.set(drag, { opacity: 0 });
+            // enable the next draggable
+            if (index < paths.length - 1) {
+              draggable[index + 1].enable();
+            }
+          }
+        },
+      });
+
+      /// LOOP END
+    }
+
+    // disable all but first draggable
+    for (let i = 1; i < paths.length; i++) {
+      draggable[i].disable();
+    }
 
     gsap.set(".container", {
       autoAlpha: 1,
     });
 
-    function pointModifier(point) {
-      var p = closestPoint(path, pathLength, point);
-
-      gsap.set(drag, {
-        rotation: p.rotation,
-      });
-
-      lastPoint.x = point.x;
-      lastPoint.y = point.y;
-      lastLength = p.length;
-
-      drawTween.progress(lastLength / pathLength);
-
-      return p.point;
-    }
-
-    function closestPoint(pathNode, pathLength, point) {
+    function closestPoint(pathNode, pathLength, point, lastPoint, lastLength) {
+      // convert to pure function
       var precision = 1,
         best,
         bestLength,
         bestDistance = Infinity;
-      var traveled = distance2sqrt(lastPoint);
+      var traveled = distance2sqrt(lastPoint, point);
       var scanFrom = lastLength - traveled;
       var scanTo = lastLength + traveled;
       scanFrom = scanFrom < 0 ? 0 : scanFrom;
@@ -92,7 +134,8 @@
       ) {
         if (
           (scanDistance = distance2(
-            (scan = pathNode.getPointAtLength(scanLength))
+            (scan = pathNode.getPointAtLength(scanLength)),
+            point
           )) < bestDistance
         ) {
           (best = scan),
@@ -110,13 +153,15 @@
         length: bestLength,
       };
 
-      function distance2(p) {
+      function distance2(p, point) {
+        // convert to pure function
         var dx = p.x - point.x,
           dy = p.y - point.y;
         return dx * dx + dy * dy;
       }
 
-      function distance2sqrt(p) {
+      function distance2sqrt(p, point) {
+        // convert to pure function
         var dx = p.x - point.x,
           dy = p.y - point.y;
         return Math.sqrt(dx * dx + dy * dy);
@@ -139,8 +184,11 @@
       viewBox="-20 -20 440 420"
       xmlns="http://www.w3.org/2000/svg"
     >
-      {#each paths as path}
-        <path id="path" class="path" d={path} />
+      {#each paths as path, index}
+        <g id="goal" bind:this={goalElements[index]}>
+          <circle class="goal" cx="15" cy="15" r="15" />
+        </g>
+        <path id="path" class="path" d={path} bind:this={pathElements[index]} />
 
         <path
           stroke="#88CE02"
@@ -148,17 +196,24 @@
           id="drawMe"
           class="path"
           d={path}
+          bind:this={lineElements[index]}
         />
+
+        <g id="drag" bind:this={dragElements[index]}>
+          <circle class="oval" cx="15" cy="15" r="15" />
+          <polygon class="arrow" points="10,10 25,15 10,20" />
+        </g>
       {/each}
-      <g id="drag">
-        <circle class="oval" cx="15" cy="15" r="15" />
-        <polygon class="arrow" points="10,10 25,15 10,20" />
-      </g>
     </svg>
   </div>
 </main>
 
 <style>
+  #obj {
+    background-color: red;
+    opacity: 0.3;
+  }
+
   .container {
     visibility: hidden;
     opacity: 0;
@@ -168,6 +223,9 @@
     fill: #f5a623;
   }
 
+  .goal {
+    fill: red;
+  }
   .arrow {
     fill: #fff;
   }
