954b03d7756192b7d8b933282a8654a6c3137bdb
jcm2512 <joe.magalotti@gmail.com>
2022-03-25 00:28:55 +0900

    back button closes the game

--- src/store.js
+++ src/store.js
@@ -2,3 +2,6 @@ import { writable } from "svelte/store";
 
 export const gameLoaded = writable(false);
 export const gameLoaderWordId = writable(0);
+export const gameWordLimit = writable(5);
+
+// TODO: #11 store auto subscription using $
