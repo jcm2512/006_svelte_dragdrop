5a9e40b9a3720933133e9aa9cd066da81139806b
jcm2512 <joe.magalotti@gmail.com>
2022-03-28 21:56:36 +0900

    3d tiles

--- src/Home.svelte
+++ src/Home.svelte
@@ -44,13 +44,13 @@
       {#each words as tile}
         {#if $cvcObject[tile].unlocked}
           <div
-            class="tile"
+            class="button"
             style="--variable: {($cvcObject[tile].exp + 40) / 100}"
           >
             <img src={$cvcObject[tile].img} width="80" height="80" />
           </div>
         {:else}
-          <div class="tile" style="opacity: 0.1">
+          <div class="tile">
             <img src={$cvcObject[tile].img} width="80" height="80" />
           </div>
         {/if}
@@ -80,5 +80,65 @@
     justify-content: center;
     border: 1px solid black;
     border-radius: 2rem;
+  }
+
+  .button {
+    padding-top: 10px;
+    height: 90px;
+    width: 100px;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+    outline: none;
+    border: 0;
+    vertical-align: middle;
+    text-decoration: none;
+    font-size: inherit;
+    font-family: inherit;
+
+    color: hsl(0, 0%, 18%);
+    background: hsl(0, 0%, 97%);
+    border: 2px solid hsl(0, 0%, 61%);
+    border-radius: 0.75em;
+    transform-style: preserve-3d;
+    transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
+      background 150ms cubic-bezier(0, 0, 0.58, 1);
+  }
+
+  .button::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: hsl(0, 0%, 87%);
+    border-radius: inherit;
+    box-shadow: 0 0 0 2px hsl(0, 0%, 61%), 0 0.625em 0 0 hsl(0, 0%, 94%);
+    transform: translate3d(0, 0.75em, -1em);
+    transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
+      box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
+  }
+
+  .button:hover {
+    background: hsl(0, 0%, 96%);
+    transform: translate(0, 0.25em);
+  }
+  .button:hover::before {
+    box-shadow: 0 0 0 2px hsl(0, 0%, 61%), 0 0.5em 0 0 hsl(0, 0%, 94%);
+    transform: translate3d(0, 0.5em, -1em);
+  }
+  .button:active {
+    background: hsl(0, 0%, 96%);
+    transform: translate(0em, 0.75em);
+  }
+  .button:active::before {
+    box-shadow: 0 0 0 2px hsl(0, 0%, 61%), 0 0 hsl(0, 0%, 94%);
+    transform: translate3d(0, 0, -1em);
   }
 </style>
