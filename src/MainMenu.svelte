32687a641849f266898a5b019ecb6e0b026b3664
jcm2512 <joe.magalotti@gmail.com>
2022-04-26 09:22:52 +0900

    updated grid layouts

--- src/MainMenu.svelte
+++ src/MainMenu.svelte
@@ -5,13 +5,27 @@
     cvcObject,
     currentWordProgress,
   } from "./store.js";
+  import { fly } from "svelte/transition";
+  import { Swipe, SwipeItem } from "svelte-swipe";
   import GameLoader from "./GameLoader.svelte";
-  const version = "v0.2.2";
+
+  const version = "v0.3.1";
 
   let words;
   let cvcs = Object.keys($cvcObject);
 
-  $: stageCard = false;
+  let cards = ["card a", "card b", "card c", "card d"],
+    cardId = 0;
+
+  let transitionIn, transitionOut;
+
+  const swipeConfig = {
+    autoplay: false,
+    delay: 2000,
+    showIndicators: true,
+    transitionDuration: 1000,
+    defaultIndex: 0,
+  };
 
   // DEV
   $gameLoaded = false;
@@ -25,37 +39,57 @@
   });
 
   function handlePlay() {
+    transitionOut = { duration: 0 };
+
     $gameLoaded = true;
     $gameLoaderWordId = 0; // reset word ID to 0
     $currentWordProgress = 0;
   }
+  function handleNext() {
+    transitionIn = { x: -window.screen.width, duration: 500 };
+    transitionOut = { x: window.screen.width, duration: 500 };
+    if (cardId >= 1) {
+      cardId -= 1;
+    } else {
+      cardId = cards.length - 1;
+    }
+  }
 
-  function handleStage() {
-    stageCard = true;
+  function handlePrev() {
+    transitionIn = { x: window.screen.width, duration: 500 };
+    transitionOut = { x: -window.screen.width, duration: 500 };
+    if (cardId < cards.length - 1) {
+      cardId += 1;
+    } else {
+      cardId = 0;
+    }
   }
 </script>
 
-<main>
+<main class="full_grid">
   {#if $gameLoaded == true}
     <GameLoader GameWords={wordObjects} />
   {:else}
-    {#if stageCard == true}
-      <div class="stageSelect">
-        <div class="stage_card auto rounded card" />
-        <div class="back auto arrow_btn">«</div>
-        <div class="next auto arrow_btn">»</div>
-        <div class="play_btn auto rounded button" on:click={() => handlePlay()}>
-          play
-        </div>
+    <div id="level" class="auto rounded label">Player Level</div>
+    <div id="points" class="auto rounded label">Player Points</div>
+    {#key cardId}
+      <div
+        id="stage_card"
+        class="auto rounded card"
+        in:fly={transitionIn}
+        out:fly={transitionOut}
+      >
+        {cards[cardId]}
       </div>
-    {/if}
-    <div class="level auto rounded label">Player Level</div>
-    <div class="points auto rounded label">Player Points</div>
-    <div class="play_btn auto rounded button" on:click={() => handlePlay()}>
+    {/key}
+    <div id="prev" class="auto arrow_btn" on:click={() => handlePrev()}>«</div>
+    <div id="next" class="auto arrow_btn" on:click={() => handleNext()}>»</div>
+    <div
+      id="play_btn"
+      class="auto rounded button"
+      on:click={() => handlePlay()}
+    >
       play
-    </div>
-    <div class="stage_btn auto rounded button" on:click={() => handleStage()}>
-      stage
     </div>
   {/if}
 </main>
@@ -64,16 +98,7 @@
   main {
     display: grid;
     text-align: center;
-    grid-template-columns: repeat(12, 1fr);
-    grid-template-rows: repeat(12, 1fr);
-    width: 100vw;
-    height: 100vh;
     background-color: #00a4ff;
-  }
-
-  .stage_card {
-    grid-row: 4/-4;
-    grid-column: 2/-1;
   }
 
   .auto {
@@ -85,18 +110,11 @@
     justify-items: center;
   }
 
-  .next {
-    grid-column: -1/-1;
-    grid-row: 6;
-  }
-  .back {
-    grid-column: 1/1;
-    grid-row: 6;
-  }
-  .arrow_btn {
-    color: white;
-    font-size: 3rem;
-    cursor: pointer;
+  .full_grid {
+    grid-template-columns: repeat(10, 10vw);
+    grid-template-rows: repeat(16, 6.25vh);
+    width: 100vw;
+    height: 100vh;
   }
 
   .rounded {
@@ -106,49 +124,57 @@
 
   .card {
     background-color: #ff61aa;
+    z-index: 50;
   }
 
   .button {
     cursor: pointer;
     background-color: #f4d042;
+    z-index: 10;
   }
 
   .label {
     background-color: #ffffff;
+    z-index: 10;
   }
 
-  .level {
+  .arrow_btn {
+    color: white;
+    font-size: 3rem;
+    cursor: pointer;
+    z-index: 100;
+  }
+
+  #play_btn {
+    grid-row: 15/16;
+    grid-column: 4/8;
+  }
+
+  #stage_card {
+    grid-row: 4/-4;
+    grid-column: 2/-2;
+  }
+
+  #next,
+  #prev {
+    grid-row: 8/11;
+  }
+
+  #prev {
+    grid-column: 1;
+  }
+
+  #next {
+    grid-column: -2;
+  }
+
+  #level {
     grid-row: 2/3;
     grid-column: 2/4;
   }
 
-  .points {
+  #points {
     grid-row: 2/3;
     grid-column: 5/-2;
-  }
-
-  .stage_btn {
-    grid-row: 11/12;
-    grid-column: 2/4;
-    z-index: 10;
-  }
-
-  .play_btn {
-    grid-row: 11/12;
-    grid-column: 5/9;
-    z-index: 10;
-  }
-
-  .stageSelect {
-    display: grid;
-    text-align: center;
-    grid-template-columns: repeat(12, 1fr);
-    grid-template-rows: repeat(12, 1fr);
-    width: 100vw;
-    height: 100vh;
-    background-color: hsla(0, 0%, 0%, 0.7);
-    grid-row: 1/-1;
-    grid-column: 1/-1;
-    z-index: 50;
   }
 </style>
