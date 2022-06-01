c078fc98bab6a1f507b30f773e792e8e64e0bb2e
jcm2512 <joe.magalotti@gmail.com>
2022-06-01 19:08:53 +0900

    Gatcha working

--- src/GatchaMenu.svelte
+++ src/GatchaMenu.svelte
@@ -1,42 +1,37 @@
 <script>
-  import { cvcObject } from "./store.js";
-  import { animateCSS } from "./animateCSS.svelte";
-  import ProgressBar from "./components/ProgressBar.svelte";
-  import Stars from "./Stars.svelte";
+  import { cvcObject, gatchaMenu, gameStars } from "./store.js";
+  import Button from "./components/Button.svelte";
 
-  export let index;
-  let GameLevel = Object.keys($cvcObject)[index];
-  let cvcs = Object.keys($cvcObject[GameLevel]);
-  let DOMelements = [];
+  export let LockedWords;
+  export let GameLevel;
+
+  let gatcha = function () {
+    if ($gameStars.stars >= 5) {
+      let r = Math.floor(Math.random() * LockedWords.length);
+      $cvcObject[GameLevel][LockedWords[r]].status = "unlocked";
+      $gameStars.stars -= 5;
+      $gatchaMenu = false;
+    } else {
+      console.log("Not enough stars");
+    }
+  };
 </script>
 
-<li class="card container">
+<div class="main card container">
   <img src="/assets/ui/gumball.png" class="gatcha" alt="Gatcha" />
-</li>
+  <div class="_button">
+    <Button
+      label="go"
+      grid_column="1"
+      grid_row="-2"
+      button_function={gatcha}
+      bg_color="var(--green)"
+    />
+  </div>
+</div>
 
 <style>
-  .img_Container {
-    grid-column: span 2;
-    display: grid;
-    align-items: center;
-    justify-items: center;
-  }
-
-  /* Dealing with 2 orphan items */
-  .img_Container:last-child:nth-child(3n - 1) {
-    grid-column-end: -2;
-  }
-
-  .img_Container:nth-last-child(2):nth-child(3n + 1) {
-    grid-column-end: 4;
-  }
-
-  /* Dealing with single orphan */
-  .img_Container:last-child:nth-child(3n - 2) {
-    grid-column-end: 5;
-  }
-
-  li {
+  .main {
     scroll-snap-align: center;
     display: inline-block; /* remove list bullets*/
     font-size: 2rem;
@@ -46,25 +41,15 @@
   img {
     height: 50vw;
     width: 50vw;
-    /* max-width: 3.5rem;
-    max-height: 3.5rem; */
     grid-column: 1/-1;
     grid-row: 1/-1;
   }
 
-  .locked {
-    filter: brightness(0.1) opacity(0.35);
-  }
-
-  .lock {
-    z-index: 100;
-    height: 10vw;
-    width: 10vw;
-  }
-
   .container {
     display: grid;
-    grid-template-columns: repeat(6, 1fr);
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 4fr 1fr;
     justify-content: space-evenly;
     align-content: space-evenly;
     width: 100%;
@@ -80,5 +65,12 @@
     background-color: var(--yellow);
     z-index: 50;
     border: var(--border-width) solid var(--white);
+  }
+
+  ._button {
+    grid-row: -2;
+    grid-column: 1;
+    width: 80%;
+    height: 80%;
   }
 </style>
